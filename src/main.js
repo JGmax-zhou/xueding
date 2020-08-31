81c957bdeab71f4c477c5c9f224fb2d7e2cdcf87
JGmax-zhou <573682554@qq.com>
2020-08-31 23:07:59 +0800

    底部头部-周建国-8/31

--- src/main.js
+++ src/main.js
@@ -7,6 +7,26 @@ import "amfe-flexible";
 import './assets/style/reset.scss'
 import './assets/style/var.scss'
 
+// 依赖组件
+import {
+    Tabbar,
+    TabbarItem,
+    Icon,
+    NavBar,
+    Swipe,
+    SwipeItem,
+    Sidebar,
+    SidebarItem
+} from 'vant';
+Vue.use(Tabbar);
+Vue.use(TabbarItem);
+Vue.use(Icon);
+Vue.use(NavBar);
+Vue.use(Swipe);
+Vue.use(SwipeItem);
+Vue.use(Sidebar);
+Vue.use(SidebarItem);
+
 Vue.config.productionTip = false
 
 new Vue({
